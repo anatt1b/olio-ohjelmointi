6c10a68e850f231692a2e0489b36846b237bf46f
Antti <antti.buller@gmail.com>
2025-10-27 18:59:29 +0200

    H1_2 valmis

--- H1_2/main.cpp
+++ H1_2/main.cpp
@@ -2,8 +2,22 @@
 
 using namespace std;
 
+// funktio mikä laskee luvun neliön
+int laskeNelio(int luku)
+{
+    int tulos = luku * luku;
+    return tulos;
+}
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    int luku;
+    cout << "Anna luku" << endl;
+    cin >> luku;
+
+    int nelio = laskeNelio(luku);
+
+    cout << "Luvun " <<luku<< " neliö on " <<nelio<< endl;
+
     return 0;
 }
