519d9cc03a5dc31c53f054b63305200c3d740871
Antti <antti.buller@gmail.com>
2025-11-10 17:31:53 +0200

    added assignment to h1

--- homework/h1/main.cpp
+++ homework/h1/main.cpp
@@ -1,3 +1,19 @@
+/*
+Muokkaa main-funktiota seuraavasti:
+    Kysy luvut ja tallenna ne muuttujiin a ja b
+    käytä cin ja cout olioita
+
+Lisää sovellukseen summan ja osamäärän laskemiseen ja tulostamiseen käytettävät funktiot:
+    funktio calcSum ottaa vastaan kaksi kokonaislukua ja tulostaa niiden summan, mutta ei palauta mitään.
+    funktio calcDiv ottaa vastaan kaksi kokonaislukua ja tulostaa niiden osamäärän, mutta ei palauta mitään.
+    jos jakaja on nolla calcDiv tulostaa virheilmoituksen.
+    kutsu em. funktioita mainissa antamalla niille argumenttina muuttujat a ja b.
+
+Lisää sovellukseen toiset summan ja osamäärän laskemiseen ja tulostamiseen käytettävät funktiot:
+    funktio retSum ottaa vastaan kaksi kokonaislukua ja palauttaa niiden summan, mutta ei tulosta mitään
+    funktio retDiv ottaa vastaan kaksi kokonaisluka ja palauttaa niiden osamäärän (float), mutta ei tulosta mitään
+*/
+
 #include <iostream>
 #include <cmath>
 
